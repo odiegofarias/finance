27aadcebc8e9589a9b6a5fbdca8ddc00e61e7bf5
odiegofarias <diego.farias.fernandes@gmail.com>
2023-07-04 22:07:34 -0300

    Configs básicas de template e criação da view gerenciar

--- core/settings.py
+++ core/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = BASE_DIR / 'templates/static'
+STATICFILES_DIRS = [BASE_DIR / 'templates/static']
 STATIC_ROOT = 'static'
 
 MEDIA_ROOT = BASE_DIR / 'media'
--- perfil/urls.py
+++ perfil/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('home/', views.home, name='home')
+    path('home/', views.home, name='home'),
+    path('gerenciar/', views.gerenciar, name='gerenciar'),
+
 ]
