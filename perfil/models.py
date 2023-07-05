9acced6a65e0164bdb3e05dd618aa7c4c95e6272
odiegofarias <diego.farias.fernandes@gmail.com>
2023-07-05 19:46:46 -0300

    Algumas funções do CRUD

--- perfil/models.py
+++ perfil/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Categoria(models.Model):
+    categoria = models.CharField(max_length=25)
+    essencial = models.BooleanField(default=False)
+    valor_planejamento = models.DecimalField(default=0, max_digits=8, decimal_places=2)
+
+    def __str__(self) -> str:
+        return self.categoria
+    
+
+BANCO_CHOICES = (
+    ('NU', 'Nubank'),
+    ('CE', 'Caixa Econômica'),
+    ('IT', 'Itaú'),
+    ('BB', 'Banco do Brasil'),
+)
+
+TIPO_CHOICES = (
+    ('PF', 'Pessoa Física'),
+    ('PJ', 'Pessoa Jurídica'),
+)
+
+
+class Conta(models.Model):
+    apelido = models.CharField(max_length=25)
+    banco = models.CharField(max_length=2 ,choices=BANCO_CHOICES)
+    tipo = models.CharField(max_length=2, choices=TIPO_CHOICES)
+    valor = models.DecimalField(default=0, max_digits=8, decimal_places=2)
+    icone = models.ImageField(upload_to='icones')
+
+    def __str__(self) -> str:
+        return self.apelido
--- perfil/views.py
+++ perfil/views.py
@@ -1,8 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib import messages
+from django.contrib.messages import constants
+from django.db.models import Sum
+from django.http import HttpResponse
+from .models import Conta
+
 
 
 def home(request):
     return render(request, 'home.html')
 
 def gerenciar(request):
-    return render(request, 'gerenciar.html')
+    contas = Conta.objects.all()
+    total_contas = contas.aggregate(Sum('valor'))['valor__sum']
+    # total_contas = 0
+
+    # for conta in contas:
+    #     total_contas += conta.valor
+
+    return render(request, 'gerenciar.html', {'contas': contas, 'total_contas': total_contas})
+
+def cadastrar_banco(request):
+    # Puxa do atributo name
+    apelido = request.POST.get('apelido')
+    banco = request.POST.get('banco')
+    tipo = request.POST.get('tipo')
+    valor = request.POST.get('valor')
+    icone = request.FILES.get('icone')
+
+    if len(apelido.strip()) == 0 or len(valor.strip()) == 0:
+        messages.add_message(request, constants.ERROR, 'Dados incorretos. Preencha novamente.')
+        return redirect(reverse('gerenciar'))
+        
+
+
+    conta = Conta(
+        apelido = apelido,
+        banco = banco,
+        tipo = tipo,
+        valor = valor,
+        icone = icone
+    )
+
+    conta.save()
+
+    messages.add_message(request, constants.SUCCESS, 'Conta adicionada com sucesso.')
+    return redirect(reverse('gerenciar'))
+
+def deletar_banco(request, id):
+    try:
+        conta = Conta.objects.get(id=id)
+        conta.delete()
+    except Conta.DoesNotExist:
+        return HttpResponse('Não encontrado')
+
+    return redirect(reverse('gerenciar'))
